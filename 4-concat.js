5ca5c9c3dbfa936c4fcbdb5e37583a1f8f8aa14c
Mohamed Dahrour <116980687+dsaihmrour@users.noreply.github.com>
2025-05-22 17:29:09 +0100

    4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,8 +1,3 @@
 const args = process.argv.slice(2);
 
-if (args.length < 2) {
-  console.log("Please provide two arguments.");
-} else {
-  const [first, second] = args;
-  console.log(`${first} is ${second}`);
-}
+console.log(`${args[0]} is ${args[1]}`);
