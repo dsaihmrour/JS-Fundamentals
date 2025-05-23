71966f01fc40170a450960a1a4f2dd49c7cd0a92
Mohamed Dahrour <116980687+dsaihmrour@users.noreply.github.com>
2025-05-23 15:13:02 +0100

    3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,3 @@
 const args = process.argv.slice(2);
-let message = "";
 
-if (args.length === 0) {
-  message = "No argument";
-} else {
-  message = "School";
-}
-
-console.log(message);
+console.log(args[0] || "No argument");
